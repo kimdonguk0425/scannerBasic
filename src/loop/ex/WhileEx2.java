f4a1f1b9706668853c6b11eba9c6b80537d1f8a8
김동욱 <qwehddnr@gmail.com>
2025-03-23 17:13:35 +0900

    WhileLoopBasic

---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,14 @@
+package loop.ex;
+
+public class WhileEx2 {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        int num = 0;
+        while (count <= 10) {
+            System.out.println(num+=2);
+            count++;
+        }
+    }
+}
---
+++ src/loop/ex/WhileEx3.java
@@ -0,0 +1,14 @@
+package loop.ex;
+
+public class WhileEx3 {
+
+    public static void main(String[] args) {
+
+        int max = 0;
+        int a = 100;
+        for (int i = 0; i <= a; i++) {
+            max += i;
+        }
+        System.out.println(max);
+    }
+}
